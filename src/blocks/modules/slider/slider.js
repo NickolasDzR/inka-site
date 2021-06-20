1e6406e04ef18558fc056e894728f405d5884408
NickolasDzR <nikolyae2019@gmail.com>
2021-06-20 19:43:58 +0300

    slider

---
+++ src/blocks/modules/slider/slider.js
@@ -0,0 +1,6 @@
+import Glide from '@glidejs/glide'
+
+new Glide('.glide', {
+    type: 'carousel',
+    perView: 1,
+}).mount()
--- src/blocks/modules/solution/solution.js
+++ src/blocks/modules/solution/solution.js
@@ -16,51 +16,12 @@ const solutionSection = document.querySelector(".solution"),
     solutionItemHeight = solutionItemsWrp[0].offsetHeight,
     solutionFixed = document.querySelector(".solution__fixed-items");
 
-// const isObserved = (entry) => {
-//     if (entry[0].intersectionRatio > 0) {
-//
-//         console.log(entry[0].intersectionRatio)
-//
-//         solutionFixed.style.cssText = "position: fixed; top: 40%;"
-//         const index = parseInt(entry[0].target.closest(".solution__item").dataset.index);
-//         imagesDesktop[index].classList.add("solution__img-fixed_show");
-//
-//         imagesDesktop.forEach((el, i) => {
-//             if (el.classList.contains("solution__img-fixed_show") && index !== i) {
-//                 el.classList.remove("solution__img-fixed_show");
-//             }
-//         });
-//     }
-// }
-//
-// solutionItemsWrp.forEach((el, index) => {
-//
-//     function observeFunc() {
-//         const wh = window.innerHeight;
-//         const rootMarginSettings = `${ Math.round(parseInt(window.innerHeight) / 2)}`;
-//         let settings;
-//
-//         settings = {
-//             rootMargin: `-130px 0px -130px 0px`,
-//             threshold: 0,
-//         }
-//
-//         console.log(settings, index);
-//
-//         const observer = new IntersectionObserver(isObserved, settings);
-//         observer.observe(el)
-//     }
-//
-//     observeFunc();
-// })
-
 const solutionItemsWrpPosition = solutionItemsWrp[0].offsetTop,
     solutionItemsWrpOffsetTop = [],
-    coordinatsFixedTopFirstElement = solutionItems[0].offsetTop;
-coordinatsFixedTopLastElement = solutionItems[solutionItems.length - 1].offsetTop,
-    imageCoordination = imagesDesktop[0].getBoundingClientRect().left;
+    coordinatsFixedTopFirstElement = solutionItems[0].offsetTop,
+    coordinatsFixedTopLastElement = solutionItems[solutionItems.length - 1].offsetTop;
 
-console.log(imageCoordination);
+let imageCoordination = imagesDesktop[0].getBoundingClientRect().left;
 
 solutionItemsWrp.forEach(el => {
     solutionItemsWrpOffsetTop.push(el.offsetTop);
@@ -80,15 +41,15 @@ document.addEventListener("scroll", () => {
     const wh = window.innerHeight;
     const centerWindow = Math.round(scrolledFromTop + (wh / 2))
 
-
-    if ((solutionOffsetTop + solutionItemsWrpOffsetTop[0]) - centerWindow > 0) {
+        console.log((solutionOffsetTop + solutionItemsWrpOffsetTop[0]) - centerWindow);
+    if ((solutionOffsetTop + solutionItemsWrpOffsetTop[0]) - centerWindow > -121) {
         solutionFixed.style.cssText = "top: 0; bottom: auto;"
     } else if ((solutionOffsetTop + solutionItemsWrpOffsetTop[solutionItemsWrpOffsetTop.length - 1]) - centerWindow < -(solutionItemHeight - 60)) {
         imagesDesktop[imagesDesktop.length - 1].style.cssText = "bottom: 0; top: auto;";
         solutionFixed.style.cssText = ""
     } else {
         solutionItemsWrpOffsetTop.forEach((el, index) => {
-            if ((solutionOffsetTop + solutionItemsWrpOffsetTop[index]) - centerWindow < 100) {
+            if ((solutionOffsetTop + solutionItemsWrpOffsetTop[index]) - centerWindow < -100) {
                 solutionFixed.style.cssText = `position: fixed; 
                                                 transform: translate3d(${imageCoordination}px, 37.5%, 0px);`
                 deleteImageClass(index);
