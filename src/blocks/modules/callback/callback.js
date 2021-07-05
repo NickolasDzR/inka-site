714f7d7ddc4181448ff9d01f161f1d3f65beb277
NickolasDzR <nikolyae2019@gmail.com>
2021-07-05 19:20:46 +0300

    fetch fixed

--- src/blocks/modules/callback/callback.js
+++ src/blocks/modules/callback/callback.js
@@ -3,71 +3,36 @@ const url = "http://inka.finance:90/api/subscribe";
 const btn = document.querySelector(".button");
 const formError = document.querySelector(".callback__form").dataset.error;
 
-// async function sendData(formData) {
-//     try {
-//
-//         let response = fetch(url, {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: formData
-//         });
-//
-//         var data = await response.json(),
-//             contentType = response.headers.get("content-type"),
-//             status = response.status;
-//
-//         if (contentType && contentType.indexOf("application/json") !== -1 && status === 200 && data.status === "error" || data.status === "Success!") {
-//             if (data.status === "Success!") {
-//                 messageHandler(data.message, data.status);
-//             }
-//             if (data.status === "error") {
-//                 messageHandler(data.message, data.status);
-//             }
-//         } else if (status === 419) {
-//             alert("Пожалуйста, повторите отправку данных еще раз после перезагрузки страницы");
-//             window.location.reload();
-//             return false;
-//         } else {
-//             return alert(formError);
-//         }
-//     } catch {
-//         console.log("erorr");
-//     }
-// }
-
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//     .then(response => response.json())
-//     .then(json => console.log(json))
-
 async function sendData(formData) {
-    await fetch("http://inka.finance:90/api/subscribe", {
+    await fetch(url, {
         method: "POST",
         headers: {
-            "Content-type": "application/json"
+            "Accept": "application/json",
+            "Content-Type": "application/json"
         },
-        mode: "no-cors",
-        body: JSON.stringify({"email": "email", "user": "user"}),
+        body: JSON.stringify(formData)
     }).then(data => {
 
-        const contentType = data.headers.get("content-type"),
-            status = data.status;
+        const response = data.json();
 
-        if (contentType && contentType.indexOf("application/json") !== -1 && status === 200 && data.status === "error" || data.status === "Success!") {
-            if (data.status === "Success!") {
-                messageHandler(data.message, data.status);
+        response.then(value => {
+            const code = value.code;
+
+            if (code === 200 || code === 409) {
+                if (value.status === "Success!") {
+                    messageHandler(value.message, "Success!");
+                }
+                if (code === 409) {
+                    messageHandler(value.errors[0].message, "error");
+                }
+            } else if (status === 419) {
+                alert("Пожалуйста, повторите отправку данных еще раз после перезагрузки страницы");
+                window.location.reload();
+                return false;
+            } else {
+                return alert(formError);
             }
-            if (data.status === "error") {
-                messageHandler(data.message, data.status);
-            }
-        } else if (status === 419) {
-            alert("Пожалуйста, повторите отправку данных еще раз после перезагрузки страницы");
-            window.location.reload();
-            return false;
-        } else {
-            return alert(formError);
-        }
+        });
     }).catch(error => {
         console.log(error);
     });
@@ -75,19 +40,23 @@ async function sendData(formData) {
 
 const messageHandler = (message, type) => {
     let messageAnswer;
+    let footerHeading = document.querySelector(".footer__heading");
 
     if (type === "error") {
-        messageAnswer = "Please, enter a valid E-mail address";
+        messageAnswer = message;
         footerHeading.classList.add("footer__heading_error");
     }
     if (type === "Success!") {
         messageAnswer = message;
+        if (footerHeading.classList.contains("footer__heading_error")) {
+            footerHeading.classList.remove("footer__heading_error");
+        }
     }
     if (type === "valid error") {
         messageAnswer = message;
     }
 
-    const footerHeading = document.querySelector(".footer__heading");
+    footerHeading = document.querySelector(".footer__heading");
     return footerHeading.innerHTML = messageAnswer;
 };
 
@@ -95,10 +64,10 @@ btn.addEventListener("click", (e) => {
     e.preventDefault();
 
     const inputs = e.target.closest("form").querySelectorAll("input");
-    let inputsData = new FormData();
+    let inputsData = {};
 
     inputs.forEach((el, index) => {
-        inputsData.append(inputs[index].type, inputs[index].value);
+        inputsData[inputs[index].dataset.type] = inputs[index].value;
     });
 
     return sendData(inputsData);
